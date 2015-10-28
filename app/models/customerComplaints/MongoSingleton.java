00ab526c7559f0a8735ab75411ef16231b59f471
Vivek D K <vivek.dk@wearexenon.com>
2015-10-28 18:12:26 +0530

    changes in mongo endpoint

--- app/models/customerComplaints/MongoSingleton.java
+++ app/models/customerComplaints/MongoSingleton.java
@@ -3,14 +3,22 @@ package com.redmart.assignment.complaints;
 import org.mongodb.morphia.*;
 import org.mongodb.morphia.query.*;
 
-import com.mongodb.MongoClient;
+import com.mongodb.*;
+import java.util.*;
 
 public class MongoSingleton {
 
 	private final static Morphia morphia = new Morphia();
-	private final static Datastore datastore = morphia.createDatastore(new MongoClient(), "redmart");
+	private static Datastore datastore = null;
 	
-	public static Datastore getMongoDS (){
-	  return datastore;
-	}
+	public static synchronized Datastore getMongoDS() {
+        if (datastore == null) {
+			MongoClientURI connectionString=new MongoClientURI("mongodb://redmart:redmart@ds045714.mongolab.com:45714/heroku_t6jxc42d");
+			MongoClient mongoClient=new MongoClient(connectionString);
+			Morphia morphia=new Morphia();
+			// morphia.mapPackage("com.redmart.assignment.complaints");
+			datastore=morphia.createDatastore(mongoClient,"heroku_t6jxc42d");	
+        }
+        return datastore;
+    }	
 }
